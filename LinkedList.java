f524ba4a564297c6e5b4260bb8aafd0f88a87fc8
soosue <54942017+soosue@users.noreply.github.com>
2019-09-08 18:04:24 +0900

    LinkedList.java 수정
    원하는 위치 append, 삭제할 데이터 없을 때 delete 추가

--- LinkedList.java
+++ LinkedList.java
@@ -7,9 +7,11 @@ public class LinkedList {
 		head.append(2);
 		head.append(3);
 		head.append(4);
+		head.appendFirst(7);
 		head.retrieve();
-		head.delete(2);
+		head.delete(0);
 		head.retrieve();
+		
 	}
 }
 
@@ -20,7 +22,24 @@ class Node{
 		this.data=d;
 	}
 	
-	void append(int d) {
+	void appendFirst(int d) {
+		Node n = new Node(this.data);
+		n.next=this.next;
+		this.data=d;
+		this.next=n;
+	}
+	
+	void append(int d, int pre) { // pre 뒤에 데이터 추가
+		Node nn = new Node(d);
+		Node n = this;
+		while(n.data!=pre) {
+			n=n.next;
+		}
+		nn.next=n.next;
+		n.next=nn;
+	}
+	
+	void append(int d) { //데이터 맨 끝에 추가
 		Node end = new Node(d);
 		Node n = this;
 		while(n.next!=null) {
@@ -32,8 +51,13 @@ class Node{
 		Node n=this;
 		while(n.next.data!=d) {
 			n=n.next;
+			if(n.next==null) //삭제할 데이터가 없을 때
+				break;
 		}
-		n.next=n.next.next;
+		if(n.next==null) //삭제할 데이터가 없을 때
+			System.out.println("삭제할 데이터가 없습니다.");
+		else
+			n.next=n.next.next;
 	}
 	
 	void retrieve() {
